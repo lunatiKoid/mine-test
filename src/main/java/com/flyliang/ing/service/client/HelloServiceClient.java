3b29fe86bd65914ee0ec8a1b5ebd3bb6128f82ec
liang.yaol <liang.yaol@alibaba-inc.com>
2015-06-18 21:45:56 +0800

    good code

--- src/main/java/com/flyliang/ing/service/client/HelloServiceClient.java
+++ src/main/java/com/flyliang/ing/service/client/HelloServiceClient.java
@@ -8,29 +8,35 @@ import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 
-
-
 public class HelloServiceClient {
-/**
-    * 调用 Hello 服务
-    * @param args
-    */
-   public static void main(String[] args) {
-       try {
-           // 设置调用的服务地址为本地，端口为 7911
-           TTransport transport = new TSocket("localhost", 7911);
-           transport.open();
-           // 设置传输协议为 TBinaryProtocol
-           TProtocol protocol = new TBinaryProtocol(transport);
-           Hello.Client client = new Hello.Client(protocol);
-           // 调用服务的 helloVoid 方法
-           client.helloVoid();
-           client.helloString("hello world");
-           transport.close();
-       } catch (TTransportException e) {
-           e.printStackTrace();
-       } catch (TException e) {
-           e.printStackTrace();
-       }
-   }
+
+    private static final Integer SERVER_PORT    = 7911;
+    private static final String  SERVER_ADDRESS = "localhost";
+    private static final Integer TIME_OUT       = 30000;
+
+    /**
+     * 调用 Hello 服务
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            // 设置调用的服务地址为本地
+            // TTransport transport = new TSocket(SERVER_ADDRESS, SERVER_PORT);
+            TTransport transport = new TSocket(SERVER_ADDRESS, SERVER_PORT, TIME_OUT);
+
+            transport.open();
+            // 设置传输协议为 TBinaryProtocol
+            TProtocol protocol = new TBinaryProtocol(transport);
+            Hello.Client client = new Hello.Client(protocol);
+            // 调用服务的 helloVoid 方法
+            client.helloVoid();
+            client.helloString("hello world");
+            transport.close();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+    }
 }
