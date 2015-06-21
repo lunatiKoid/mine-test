a1b119ee0192064b5cf3c321f87223388a1f042a
liang.yaol <liang.yaol@alibaba-inc.com>
2015-06-21 10:23:25 +0800

    update the pom.xml for test
    Signed-off-by: liang.yaol <liang.yaol@alibaba-inc.com>

--- src/main/java/com/flyliang/ing/service/client/HelloServiceClient.java
+++ src/main/java/com/flyliang/ing/service/client/HelloServiceClient.java
@@ -3,11 +3,24 @@ package com.flyliang.ing.service.client;
 import com.flyliang.ing.service.constants.ServerSettingConstants;
 import com.flyliang.ing.service.demo.Hello;
 import org.apache.thrift.TException;
+import org.apache.thrift.async.AsyncMethodCallback;
+import org.apache.thrift.async.TAsyncClientManager;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingSocket;
+import org.apache.thrift.transport.TNonblockingTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.concurrent.CountDownLatch;
 
 public class HelloServiceClient {
 
@@ -17,6 +30,10 @@ public class HelloServiceClient {
      * @param args
      */
     public static void main(String[] args) {
+
+    }
+
+    public static void simpleClient() {
         try {
             // 设置调用的服务地址为本地
             TTransport transport = new TSocket(ServerSettingConstants.SERVER_ADDRESS,
@@ -39,4 +56,63 @@ public class HelloServiceClient {
             e.printStackTrace();
         }
     }
+
+    public static void nonBlockingClient() {
+        try {
+            // 设置调用的服务地址为本地
+            TTransport transport = new TFramedTransport(new TSocket(ServerSettingConstants.SERVER_ADDRESS,
+                                                                    ServerSettingConstants.SERVER_PORT,
+                                                                    ServerSettingConstants.TIME_OUT));
+
+            // 设置传输协议为 TCompactProtocol
+            TProtocol tProtocol = new TCompactProtocol(transport);
+            transport.open();
+
+            Hello.Client client = new Hello.Client(tProtocol);
+
+            // 调用服务的 helloVoid 方法
+            client.helloVoid();
+            client.helloString("hello world");
+            transport.close();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static void asyncNonBlockingClient() {
+//        try {
+//            TAsyncClientManager tAsyncClientManager = new TAsyncClientManager();
+//            // 设置调用的服务地址为本地
+//            TNonblockingTransport tNonBlockingTransport = new TNonblockingSocket(ServerSettingConstants.SERVER_ADDRESS,
+//                                                                                 ServerSettingConstants.SERVER_PORT,
+//                                                                                 ServerSettingConstants.TIME_OUT);
+//            // 设置传输协议为 TCompactProtocol
+//            TProtocolFactory tProtocolFactory = new TCompactProtocol.Factory();
+//
+//
+//            Hello.AsyncClient asyncClient = new Hello.AsyncClient(tProtocolFactory,tAsyncClientManager,tNonBlockingTransport);
+//            System.out.println("client starting ....");
+//
+//            CountDownLatch latch = new CountDownLatch(1);
+//
+//            // 调用服务的 helloVoid 方法
+////            client.helloVoid();
+////            client.helloString("hello world");
+////            transport.close();
+//        } catch (TTransportException e) {
+//            e.printStackTrace();
+//        } catch (TException e) {
+//            e.printStackTrace();
+//        } catch (Exception e) {
+//
+//        }
+//    }
+
+
+//    public class AsynCallback implements AsyncMethodCallback<> {
+//
+//    }
+
 }
--- src/main/java/com/flyliang/ing/service/server/HelloServiceServer.java
+++ src/main/java/com/flyliang/ing/service/server/HelloServiceServer.java
@@ -1,16 +1,23 @@
 package com.flyliang.ing.service.server;
 
+import com.flyliang.ing.service.constants.ServerSettingConstants;
 import com.flyliang.ing.service.demo.Hello;
 import com.flyliang.ing.service.demo.HelloServiceImpl;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TBinaryProtocol.Factory;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.server.TNonblockingServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
 import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.transport.TTransportFactory;
 
 public class HelloServiceServer {
 
@@ -20,12 +27,18 @@ public class HelloServiceServer {
      * @param args
      */
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * simple singe thread server
+     */
+    public static void  simpleServer(){
         try {
             // 设置服务端口为 7911
-            TServerSocket serverTransport = new TServerSocket(7911);
+            TServerSocket serverTransport = new TServerSocket(ServerSettingConstants.SERVER_PORT);
 
             // 设置协议工厂为 TBinaryProtocol.Factory
-            Factory proFactory = new Factory();
             TProtocolFactory tProtocolFactory = new TBinaryProtocol.Factory();
 
             // 关联处理器与 Hello 服务的实现
@@ -45,4 +58,98 @@ public class HelloServiceServer {
         }
     }
 
+    /**
+     * threads pool server
+     */
+    public static  void threadPoolServer(){
+        try {
+            // 设置服务端口为 7911
+            TServerSocket serverTransport = new TServerSocket(ServerSettingConstants.SERVER_PORT);
+
+            // 设置协议工厂为 TBinaryProtocol.Factory
+            TProtocolFactory tProtocolFactory = new TBinaryProtocol.Factory();
+
+            // 关联处理器与 Hello 服务的实现
+            TProcessor processor = new Hello.Processor(new HelloServiceImpl());
+
+            // TThreadPoolServer
+            TThreadPoolServer.Args tServerArgs = new TThreadPoolServer.Args(serverTransport);
+            tServerArgs.processor(processor);
+            tServerArgs.protocolFactory(tProtocolFactory);
+
+            //
+            TServer server = new TThreadPoolServer(tServerArgs);
+            System.out.println("Start server on port 7911...");
+            server.serve();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 服务器端有客户端都需要使用相同的协议
+     * 客户端nonBlockingClient
+     * 异步客户端的服务器端结构
+     */
+    public static  void nonBlockingServer(){
+        try {
+            // 设置非阻塞服务端口为 7911
+            TNonblockingServerSocket tNonblockingServerSocket = new TNonblockingServerSocket(ServerSettingConstants.SERVER_PORT);
+
+            // 设置传输工厂为 TFramedTransport.Factory
+            TTransportFactory tTransportFactory = new TFramedTransport.Factory();
+            // 设置协议工厂为 TFramedTransport.Factory
+            TProtocolFactory tProtocolFactory = new TCompactProtocol.Factory();
+
+            // 关联处理器与 Hello 服务的实现
+            TProcessor processor = new Hello.Processor(new HelloServiceImpl());
+
+            // TNonblockingServer
+            TNonblockingServer.Args tServerArgs = new TNonblockingServer.Args(tNonblockingServerSocket);
+            tServerArgs.processor(processor);
+            // add transportFactory
+            tServerArgs.transportFactory(tTransportFactory);
+            tServerArgs.protocolFactory(tProtocolFactory);
+
+            //
+            TServer server = new TNonblockingServer(tServerArgs);
+            System.out.println("Start server on port 7911...");
+            server.serve();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 半同步半异步，客户端使用与非阻塞相同，使用传输方式TFramedTransport
+     * 客户端nonBlockingClient
+     */
+    public static  void hsHaServer(){
+        try {
+            // 设置非阻塞服务端口为 7911
+            TNonblockingServerSocket tNonblockingServerSocket = new TNonblockingServerSocket(ServerSettingConstants.SERVER_PORT);
+
+            // 设置传输工厂为 TFramedTransport.Factory
+            TTransportFactory tTransportFactory = new TFramedTransport.Factory();
+            // 设置协议工厂为 TBinaryProtocol.Factory
+            TProtocolFactory tProtocolFactory = new TBinaryProtocol.Factory();
+
+            // 关联处理器与 Hello 服务的实现
+            TProcessor processor = new Hello.Processor(new HelloServiceImpl());
+
+            // TNonblockingServer
+            THsHaServer.Args tServerArgs = new THsHaServer.Args(tNonblockingServerSocket);
+            tServerArgs.processor(processor);
+            // add transportFactory
+            tServerArgs.transportFactory(tTransportFactory);
+            tServerArgs.protocolFactory(tProtocolFactory);
+
+            // server
+            TServer server = new THsHaServer(tServerArgs);
+            System.out.println("Start server on port 7911...");
+            server.serve();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+    }
 }
